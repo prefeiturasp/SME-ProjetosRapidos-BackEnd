faf839ef3392bfddab51b10a494b739ddfa7c225
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-01-31 11:21:40 -0300

    improve: Adiciona help text no formulário de sections

--- landing_page/admin.py
+++ landing_page/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django import forms
 from landing_page.models import Section
 
 
-admin.site.register(Section)
+class SectionForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(SectionForm, self).__init__(*args, **kwargs)
+        self.fields['body'].help_text = 'Se for uma listagem, separe usando ";".'
+
+    class Meta:
+        model = Section
+        exclude = ()
+
+class MyModelAdmin(admin.ModelAdmin):
+    form = SectionForm
+
+
+admin.site.register(Section, MyModelAdmin)
